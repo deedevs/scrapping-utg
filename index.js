3353a75396e73b6e84f327c2deea91703d4ea603
David Ladipo <deedevs21@gmail.com>
2023-09-30 13:02:07 +0000

    starter to scrape website

---
+++ index.js
@@ -0,0 +1,14 @@
+// Import required libraries
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+// URL of the website you want to scrape
+const url = "https://immedi8money.com";
+
+// Function to scrape the website
+async function scrapeWebsite() {
+  console.log("Scrapping the hell out of the website");
+}
+
+// Call the function to initiate the scraping
+scrapeWebsite();
